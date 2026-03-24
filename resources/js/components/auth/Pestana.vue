92583028795ba1b0cbd1ccd1e0fa6fef113784b5
FinalBossCursoDaw <vcalvob2122@politecnics.barcelona>
2026-03-24 16:46:28 +0100

    Pestaña componente hecha

---
+++ resources/js/components/auth/Pestana.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container-background">
+  
+    <img src="/imagenes/image.png" alt="Background" class="background-image" />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Componente de pestaña autenticación
+</script>
+
