315b2ca67a4441408e908402cc7362655aec3e4e
SrHect0r <hmudarral2425@politecnics.barcelona>
2026-04-14 16:55:50 +0200

    Añadir Cliente acabado (naranjitoooooo)

--- routes/api.php
+++ routes/api.php
@@ -7,16 +7,13 @@ use App\Http\Controllers\UsuariController;
 Route::middleware('api')->group(function () {
     Route::get('/clientes-count', [ClientController::class, 'count']);
     Route::get('/clientes', [ClientController::class, 'index']);
+    Route::get('/clientes/paises', [ClientController::class, 'paises']); // DEBE IR ANTES DEL {id}
+    Route::get('/clientes/ciutats/{paisId}', [ClientController::class, 'ciutats']);
     Route::get('/clientes/{id}', [ClientController::class, 'show']);
-    //*Editar cliente*//
     Route::put('/clientes/{id}', [ClientController::class, 'update']);
-
+    Route::post('/clientes', [ClientController::class, 'store']);
 
     Route::get('/usuarios-count', [UsuariController::class, 'count']);
     Route::get('/usuarios', [UsuariController::class, 'index']);
-
-    Route::get('/paissos', [ClientController::class, 'paissos']);
-    Route::get('/ciutats/{paisId}', [ClientController::class, 'ciutats']);
-
-
 });
+
